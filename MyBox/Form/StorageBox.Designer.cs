f03bc52aa160a0d612c091a7ffec8dd561871eef
ZJX <jiaxing.zhang.ll@outlook.com>
2019-12-22 13:30:40 +0900

    2019-12-22 池袋 cafe-de-crie 不同分辨率系统下界面混乱-窗体调整

--- MyBox/Form/StorageBox.Designer.cs
+++ MyBox/Form/StorageBox.Designer.cs
@@ -28,18 +28,6 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.AutoSizeMode = System.Windows.Forms.AutoSizeMode.GrowOnly;
-            this.Font = new System.Drawing.Font(
-                "Consolas",
-                9F, System.Drawing.FontStyle.Regular,
-                System.Drawing.GraphicsUnit.Pixel, ((byte)(134))
-                );
-            //this.Font = new System.Drawing.Font(
-            //    "Microsoft Sans Serif",
-            //    12F, System.Drawing.FontStyle.Regular,
-            //    System.Drawing.GraphicsUnit.Pixel, ((byte)(134))
-            //    );
             this.groupBox1 = new System.Windows.Forms.GroupBox();
             this.label1 = new System.Windows.Forms.Label();
             this.btn_Search = new System.Windows.Forms.Button();
@@ -47,59 +35,67 @@
             this.txt_add = new System.Windows.Forms.Button();
             this.txt_typeName = new System.Windows.Forms.TextBox();
             this.groupBox2 = new System.Windows.Forms.GroupBox();
+            this.groupBox3 = new System.Windows.Forms.GroupBox();
             this.tabC_ctx = new System.Windows.Forms.TabControl();
             this.groupBox1.SuspendLayout();
             this.groupBox2.SuspendLayout();
+            this.groupBox3.SuspendLayout();
             this.SuspendLayout();
             // 
             // groupBox1
             // 
-            this.groupBox1.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left)
+            this.groupBox1.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
             | System.Windows.Forms.AnchorStyles.Right)));
             this.groupBox1.Controls.Add(this.label1);
-            this.groupBox1.Controls.Add(this.btn_Search);
-            this.groupBox1.Controls.Add(this.txt_Search);
             this.groupBox1.Controls.Add(this.txt_add);
             this.groupBox1.Controls.Add(this.txt_typeName);
-            this.groupBox1.Location = new System.Drawing.Point(12, 8);
-            this.groupBox1.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.groupBox1.Location = new System.Drawing.Point(17, 21);
+            this.groupBox1.Margin = new System.Windows.Forms.Padding(8, 12, 8, 12);
             this.groupBox1.Name = "groupBox1";
-            this.groupBox1.Padding = new System.Windows.Forms.Padding(3, 4, 3, 4);
-            this.groupBox1.Size = new System.Drawing.Size(1060, 96);
+            this.groupBox1.Padding = new System.Windows.Forms.Padding(3, 12, 8, 12);
+            this.groupBox1.Size = new System.Drawing.Size(1109, 137);
             this.groupBox1.TabIndex = 0;
             this.groupBox1.TabStop = false;
             // 
             // label1
             // 
             this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(25, 33);
+            this.label1.Location = new System.Drawing.Point(34, 50);
+            this.label1.Margin = new System.Windows.Forms.Padding(8, 0, 8, 0);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(63, 14);
+            this.label1.Size = new System.Drawing.Size(116, 28);
             this.label1.TabIndex = 4;
             this.label1.Text = "类别名称";
             // 
             // btn_Search
             // 
-            this.btn_Search.Location = new System.Drawing.Point(792, 23);
+            this.btn_Search.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.btn_Search.Location = new System.Drawing.Point(557, 50);
+            this.btn_Search.Margin = new System.Windows.Forms.Padding(8, 9, 8, 9);
             this.btn_Search.Name = "btn_Search";
-            this.btn_Search.Size = new System.Drawing.Size(75, 47);
+            this.btn_Search.Size = new System.Drawing.Size(155, 47);
             this.btn_Search.TabIndex = 3;
             this.btn_Search.Text = "查找";
             this.btn_Search.UseVisualStyleBackColor = true;
             // 
             // txt_Search
             // 
-            this.txt_Search.Location = new System.Drawing.Point(560, 30);
+            this.txt_Search.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left)));
+            this.txt_Search.Location = new System.Drawing.Point(11, 56);
+            this.txt_Search.Margin = new System.Windows.Forms.Padding(8, 9, 8, 9);
             this.txt_Search.Name = "txt_Search";
-            this.txt_Search.Size = new System.Drawing.Size(226, 22);
+            this.txt_Search.Size = new System.Drawing.Size(530, 36);
             this.txt_Search.TabIndex = 2;
             // 
             // txt_add
             // 
-            this.txt_add.Location = new System.Drawing.Point(262, 23);
-            this.txt_add.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.txt_add.Location = new System.Drawing.Point(565, 41);
+            this.txt_add.Margin = new System.Windows.Forms.Padding(8, 12, 8, 12);
             this.txt_add.Name = "txt_add";
-            this.txt_add.Size = new System.Drawing.Size(75, 47);
+            this.txt_add.Size = new System.Drawing.Size(195, 46);
             this.txt_add.TabIndex = 1;
             this.txt_add.Text = "添加";
             this.txt_add.UseVisualStyleBackColor = true;
@@ -107,52 +103,68 @@
             // 
             // txt_typeName
             // 
-            this.txt_typeName.Location = new System.Drawing.Point(94, 30);
-            this.txt_typeName.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.txt_typeName.Location = new System.Drawing.Point(171, 47);
+            this.txt_typeName.Margin = new System.Windows.Forms.Padding(8, 12, 8, 12);
             this.txt_typeName.Name = "txt_typeName";
-            this.txt_typeName.Size = new System.Drawing.Size(148, 22);
+            this.txt_typeName.Size = new System.Drawing.Size(378, 36);
             this.txt_typeName.TabIndex = 0;
             // 
             // groupBox2
             // 
-            this.groupBox2.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom)
-            | System.Windows.Forms.AnchorStyles.Left)
+            this.groupBox2.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
             | System.Windows.Forms.AnchorStyles.Right)));
             this.groupBox2.Controls.Add(this.tabC_ctx);
-            this.groupBox2.Location = new System.Drawing.Point(12, 112);
-            this.groupBox2.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.groupBox2.Location = new System.Drawing.Point(17, 182);
+            this.groupBox2.Margin = new System.Windows.Forms.Padding(8, 12, 8, 12);
             this.groupBox2.Name = "groupBox2";
-            this.groupBox2.Padding = new System.Windows.Forms.Padding(3, 4, 3, 4);
-            this.groupBox2.Size = new System.Drawing.Size(1060, 546);
+            this.groupBox2.Padding = new System.Windows.Forms.Padding(8, 12, 8, 12);
+            this.groupBox2.Size = new System.Drawing.Size(1838, 929);
             this.groupBox2.TabIndex = 1;
             this.groupBox2.TabStop = false;
             this.groupBox2.Text = "内容";
             // 
+            // groupBox3
+            // 
+            this.groupBox3.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.groupBox3.Controls.Add(this.txt_Search);
+            this.groupBox3.Controls.Add(this.btn_Search);
+            this.groupBox3.Location = new System.Drawing.Point(1137, 21);
+            this.groupBox3.Name = "groupBox3";
+            this.groupBox3.Size = new System.Drawing.Size(723, 137);
+            this.groupBox3.TabIndex = 4;
+            this.groupBox3.TabStop = false;
+            // 
             // tabC_ctx
             // 
-            this.tabC_ctx.Dock = System.Windows.Forms.DockStyle.Fill;
-            this.tabC_ctx.Location = new System.Drawing.Point(3, 19);
+            this.tabC_ctx.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.tabC_ctx.Location = new System.Drawing.Point(11, 44);
             this.tabC_ctx.Name = "tabC_ctx";
             this.tabC_ctx.SelectedIndex = 0;
-            this.tabC_ctx.Size = new System.Drawing.Size(1054, 523);
+            this.tabC_ctx.Size = new System.Drawing.Size(1816, 870);
             this.tabC_ctx.TabIndex = 0;
             // 
             // StorageBox
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 14F);
-            //this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(1084, 671);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(13F, 28F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1872, 1132);
+            this.Controls.Add(this.groupBox3);
             this.Controls.Add(this.groupBox1);
             this.Controls.Add(this.groupBox2);
-            //this.Font = new System.Drawing.Font("Consolas", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.Font = new System.Drawing.Font("Consolas", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Margin = new System.Windows.Forms.Padding(8, 12, 8, 12);
             this.MaximizeBox = false;
-            this.MinimumSize = new System.Drawing.Size(1100, 710);
             this.Name = "StorageBox";
             this.Text = "收纳箱";
             this.groupBox1.ResumeLayout(false);
             this.groupBox1.PerformLayout();
             this.groupBox2.ResumeLayout(false);
+            this.groupBox3.ResumeLayout(false);
+            this.groupBox3.PerformLayout();
             this.ResumeLayout(false);
 
         }
@@ -166,6 +178,7 @@
         private System.Windows.Forms.Button btn_Search;
         private System.Windows.Forms.TextBox txt_Search;
         private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.GroupBox groupBox3;
         private System.Windows.Forms.TabControl tabC_ctx;
     }
 }
